53347707c8457754508b2d0e9751cde7310aa9dd
Luis Henrique Justin <admin@luisjustin.com.br>
2014-07-15 00:03:11 -0300

    Estrutura Basica

---
+++ Source Codes/PHP/configs/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+#---------------------------------------#
+#--------------Auto House---------------#
+#---------------------------------------#
+$config['mysql']['host'] = "localhost";
+$config['mysql']['user'] = "root";
+$config['mysql']['pass'] = "";
+$config['mysql']['data'] = "autohouse";
+
+$config['system']['version'] = 1;
+$config['system']['checksum'] = "sadhadkasjdksad";
+?>
